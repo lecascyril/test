c41a3c7a2f95d11dd7a1d81d3aeaeee94fc5737f
Castagnet Cyril <45956179+lecascyril@users.noreply.github.com>
2022-05-04 03:41:01 +0200

    Create MerkleWhitelist.sol

---
+++ contracts/MerkleWhitelist.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract MerkleWhitelist {
+
+    bytes32 public merkleRoot;
+
+
+    constructor(bytes32 _merkleRoot) {
+        merkleRoot = _merkleRoot;
+    }
+
+    function isWhiteListed(address _account, bytes32[] calldata _proof) internal view returns(bool) {
+        return _verify(leaf(_account), _proof);
+    }
+
+    function leaf(address _account) internal pure returns(bytes32) {
+        return keccak256(abi.encodePacked(_account));
+    }
+
+    function _verify(bytes32 _leaf, bytes32[] memory _proof) internal view returns(bool) {
+        return MerkleProof.verify(_proof, merkleRoot, _leaf);
+    }
+}
